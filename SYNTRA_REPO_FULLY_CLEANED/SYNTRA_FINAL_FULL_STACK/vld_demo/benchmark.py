1dd93360e56f1534e5521f8aafcf62a9330c6d93
infektyd <77815025+infektyd@users.noreply.github.com>
2025-04-24 19:06:33 -0400

    Delete SYNTRA_REPO_FULLY_CLEANED directory
    >.>

--- SYNTRA_REPO_FULLY_CLEANED/SYNTRA_FINAL_FULL_STACK/vld_demo/benchmark.py
+++
@@ -1,8 +0,0 @@
-# Python procedural equivalent of VLD logic
-engineWarm = True
-TPSsteady = True
-crankMissing = True
-faultCode_17 = True
-
-if engineWarm and TPSsteady and crankMissing and faultCode_17:
-    print("[PY] crank.sensor.suspect = TRUE")
